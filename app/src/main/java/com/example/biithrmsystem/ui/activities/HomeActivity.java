5b10f3dafca54bd8d7cedd38c473a9cd978cbb32
mudassirsatti <dreame488@gmail.com>
2023-05-28 07:25:33 -0700

    **

--- app/src/main/java/com/example/biithrmsystem/ui/activities/HomeActivity.java
+++ app/src/main/java/com/example/biithrmsystem/ui/activities/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.biithrmsystem.ui.activities;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -18,8 +19,10 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main2);
         navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
